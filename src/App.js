9d72ae3c3bac4da0ba419c50552a92dd580c013f
nicoimpembo <130392329+nicoimpembo@users.noreply.github.com>
2023-10-02 14:44:38 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Logo del blog</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+     <header className="header">
+     <h1 ClassName="title">Logo del blog</h1>
+     <div ClassName="search-bar">
+       <input type="text" placeholder="Buscar" />
+       <button>Buscar</button>
+ </div>
+</header>
+
+  );
+}
